fa9202945d630bdd4b09befa2fbc6ad49e3d67c1
ErnstTmp <ernst.tmp@gmx.at>
2016-03-16 14:48:46 +0100

    Removed the 2nd loop over training count

--- models/UFCNN1.py
+++ models/UFCNN1.py
@@ -502,65 +502,64 @@ def train_and_predict_classification(model, sequence_length=5000, features=32, o
 #        (mean, std) = get_tradcom_normalization(filename = filename, mean = mean, std = std)
         
 
-#    for double in range(2):
-        for j in range(training_count):
-            filename = file_list[j]
-            print('Training: ',filename)
+    for j in range(training_count):
+        filename = file_list[j]
+        print('Training: ',filename)
 
-            X,y = prepare_tradcom_classification(training = True, sequence_length = sequence_length, features = features, output_dim = output_dim, filename = filename, mean = mean, std = std)
+        X,y = prepare_tradcom_classification(training = True, sequence_length = sequence_length, features = features, output_dim = output_dim, filename = filename, mean = mean, std = std)
 
-            # running over all epochs to get the optimizer working well...
-            history = model.fit({'input': X, 'output': y},
+        # running over all epochs to get the optimizer working well...
+        history = model.fit({'input': X, 'output': y},
                       verbose=1,
                       nb_epoch=epochs,
                       shuffle=False,
                       batch_size=batch_size)
-            print(history.history)
-            sys.stdout.flush()
-            final_loss = history.history['loss']
-            line.extend(final_loss)
+        print(history.history)
+        sys.stdout.flush()
+        final_loss = history.history['loss']
+        line.extend(final_loss)
 
-            save_neuralnet (model, "ufcnn_"+str(j))
+        save_neuralnet (model, "ufcnn_"+str(j))
     
-            plt.figure()
-            plt.plot(line) 
-            plt.savefig("Convergence" + str(double)+".png")
-            #plt.show()
+        plt.figure()
+        plt.plot(line) 
+        plt.savefig("Convergence" + str(double)+".png")
+        #plt.show()
 
-            total_class_count = 0
-            total_correct_class_count = 0
+        total_class_count = 0
+        total_correct_class_count = 0
 
-            for k in range(testing_count):
-                filename = file_list[training_count + k]
-                print("Predicting: ",filename)
+        for k in range(testing_count):
+            filename = file_list[training_count + k]
+            print("Predicting: ",filename)
 
-                X,y = prepare_tradcom_classification(training = False, sequence_length = sequence_length, features = features, output_dim = output_dim, filename = filename, mean = mean, std = std )
-                predicted_output = model.predict({'input': X,}, batch_size=batch_size, verbose = 2)
-                #print(predicted_output)
+            X,y = prepare_tradcom_classification(training = False, sequence_length = sequence_length, features = features, output_dim = output_dim, filename = filename, mean = mean, std = std )
+            predicted_output = model.predict({'input': X,}, batch_size=batch_size, verbose = 2)
+            #print(predicted_output)
 
-                yp = predicted_output['output']
-                xdim, ydim = yp.shape
+            yp = predicted_output['output']
+            xdim, ydim = yp.shape
     
-                ## MSE for testing
-                total_error  = 0
-                correct_class= 0
-                for i in range (xdim):
-                    delta = 0.
-                    for j in range(ydim):
-                        delta += (y[i][j] - yp[i][j]) * (y[i][j] - yp[i][j])
-                        #print ("Row %d, MSError: %8.5f " % (i, delta/ydim))
+            ## MSE for testing
+            total_error  = 0
+            correct_class= 0
+            for i in range (xdim):
+                delta = 0.
+                for j in range(ydim):
+                    delta += (y[i][j] - yp[i][j]) * (y[i][j] - yp[i][j])
+                    #print ("Row %d, MSError: %8.5f " % (i, delta/ydim))
         
-                    total_error += delta
-                    if np.argmax(y[i]) == np.argmax(yp[i]):
-                        correct_class += 1
+                total_error += delta
+                if np.argmax(y[i]) == np.argmax(yp[i]):
+                    correct_class += 1
 
-                print ("FIN Correct Class Assignment:  %6d /%7d" % (correct_class, xdim))
-                print ("FIN Final Loss:  ", final_loss)
+            print ("FIN Correct Class Assignment:  %6d /%7d" % (correct_class, xdim))
+            print ("FIN Final Loss:  ", final_loss)
 
-                total_class_count += xdim
-                total_correct_class_count += correct_class
+            total_class_count += xdim
+            total_correct_class_count += correct_class
     
-            print ("FINFIN Correct Class Assignment:  %6d /%7d" % (total_correct_class_count, total_class_count))
+        print ("FINFIN Correct Class Assignment:  %6d /%7d" % (total_correct_class_count, total_class_count))
 
     return {'model': model, 'predicted_output': predicted_output['output'], 'expected_output': y}
 
