516b04896c1df412c85c814477fe23ea7b14df81
ErnstTmp <ernst.tmp@gmx.at>
2016-04-12 21:20:04 +0200

    Batch size also in evaluate necessary

--- models/mnist_autoencoder.py
+++ models/mnist_autoencoder.py
@@ -141,6 +141,6 @@ print("Before FIT")
 
 #model.fit(X_train, Y_train, batch_size=batch_size, nb_epoch=nb_epoch, verbose=1, validation_data=(X_test, Y_test))
 model.fit(X_train, Y_train, batch_size=batch_size, nb_epoch=nb_epoch, verbose=1)
-score = model.evaluate(X_test, Y_test, verbose=0)
+score = model.evaluate(X_test, Y_test, verbose=0, batch_size=batch_size)
 print('Test score:', score[0])
 print('Test accuracy:', score[1])
